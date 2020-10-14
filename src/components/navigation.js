e408c09bc73874d8f91faf1e7761f806e7c620d2
Marie Krause <marie.krause48@web.de>
2020-10-14 20:15:32 +0200

    minor design changes

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -4,7 +4,8 @@ import ThemeChanger from "../components/themeChanger"
 
 export default (props) => (
   <nav className="navigation"> 
-    <Link to="/contact">Contact</Link>
+    <Link to="/contact">Kontakt</Link>
+    <Link to="/contact">Mein Linktree</Link>
     <ThemeChanger/>
   </nav>
   
--- src/pages/index.js
+++ src/pages/index.js
@@ -23,7 +23,7 @@ const IndexPage = ({
         <meta name="description" content={site.siteMetadata.description} />
       </Helmet>
       <HeroHeader/>
-      <h2>Blog Posts &darr;</h2>
+      <h2>Meine neuesten Abenteuer &darr;</h2>
       <div className="grids">
         {Posts}
       </div>
