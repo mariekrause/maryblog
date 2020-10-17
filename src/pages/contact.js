168836dade489342d8a4dd9c934e3585f3d4e5de
Marie Krause <marie.krause48@web.de>
2020-10-17 18:39:48 +0200

    first attempt contact functionality

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -20,22 +20,22 @@ const ContactPage = ({
           <p>Gibt es etwas Neues? Lass es mich wissen! &rarr;</p>
         </div>
         <div>
-          <form className="form-container" action="https://sendmail.w3layouts.com/SubmitContactForm" method="post">
+          <form className="form-container" action="POST" data-netlify="true">
             <div>
               <label htmlFor="w3lName">Dein Name</label>
-              <input type="text" name="w3lName" id="w3lName"/>
+              <input type="text" name="name" id="w3lName"/>
             </div>
             <div>
               <label htmlFor="w3lSender">Deine Email-Adresse</label>
-              <input type="email" name="w3lSender" id="w3lSender"/>
+              <input type="email" name="email" id="w3lSender"/>
             </div>
             <div>
               <label htmlFor="w3lSubject">Dein Betreff</label>
-              <input type="text" name="w3lSubject" id="w3lSubject"/>
+              <input type="text" name="subject" id="w3lSubject"/>
             </div>
             <div>
               <label htmlFor="w3lMessage">Deine Nachricht</label>
-              <textarea name="w3lMessage" id="w3lMessage"></textarea>
+              <textarea name="message" id="w3lMessage"></textarea>
             </div>
             <div style={{display: "flex", justifyContent: "flex-end"}}>
               <input type="submit" className="button -primary" style={{marginRight: 0}} />
