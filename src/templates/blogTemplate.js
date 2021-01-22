54d0bc4c7f1352971ee53ee32c7ef8de5e6b2227
Marie Krause <marie.krause48@web.de>
2021-01-22 23:47:31 +0100

    new author and social media display

--- src/templates/blogTemplate.js
+++ src/templates/blogTemplate.js
@@ -2,6 +2,8 @@ import React from "react"
 import Helmet from 'react-helmet';
 import { graphql } from "gatsby"
 import Layout from "../components/layout"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import '../utils/fontawesome';
 
 export default function Template({
   data, // this prop will be injected by the GraphQL query below.
@@ -27,7 +29,33 @@ export default function Template({
           {!!frontmatter.featuredimage && (
             <div className="post-featuredimage" style={{backgroundImage: `url(${frontmatter.featuredimage})`}}>
               <h1 className="post-title">{frontmatter.title}</h1>
+              <p className="post-author">von Marie Krause</p>
               <div className="post-meta">{frontmatter.date}</div>
+              <div className="post-socials">
+                <a className='icons' href='https://twitter.com/maryftf' target="_blank" rel="noreferrer">
+                  <FontAwesomeIcon icon={['fab', 'twitter']} size="1x" className="contacticon_header"/>
+                </a>
+
+                <a className='icons' href='https://instagram.com/maryftf' target="_blank" rel="noreferrer">
+                  <FontAwesomeIcon icon={['fab', 'instagram']} size="1x" className="contacticon_header"/>
+                </a>
+      
+                <a className='icons' href='https://www.linkedin.com/in/mariekr/' target="_blank" rel="noreferrer">
+                  <FontAwesomeIcon icon={['fab', 'linkedin']} size="1x" className="contacticon_header"/>
+                </a>
+
+                <a className='icons' href='https://github.com/mariekrause' target="_blank" rel="noreferrer">
+                  <FontAwesomeIcon icon={['fab', 'github-square']} size="1x" className="contacticon_header"/>
+                </a>
+                
+                <a className='icons' href='https://youtube.com/marytheidiot1' target="_blank" rel="noreferrer">
+                  <FontAwesomeIcon icon={['fab', 'youtube']} size="1x" className="contacticon_header"/>
+                </a>
+
+                <a className='icons' href='https://traewelling.de/profile/maryftf' target="_blank" rel="noreferrer">
+                  <FontAwesomeIcon icon={['fas', 'subway']} size="1x" className="contacticon_header"/>
+                </a>
+              </div>
             </div>
           )}
           <div
@@ -35,6 +63,9 @@ export default function Template({
             dangerouslySetInnerHTML={{ __html: html }}
           />
         </article>
+      </div>
+      <div className="authorBox">
+        <img src="/assets/autor.svg" className="author" alt="author"></img>
       </div>
     </Layout>
   )
